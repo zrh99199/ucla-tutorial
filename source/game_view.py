a9a76f9143587d71b0b460408dca3f46926712a5
stevezhu <zrh99199@gmail.com>
2022-06-06 22:25:25 -0700

    Extra credit fix

--- source/game_view.py
+++ source/game_view.py
@@ -8,6 +8,7 @@ from ship_sprite import ShipSprite
 from color_bar import ColorBar
 from color_manager import ColorManager
 from glow_ball import GlowBall
+from glow_line import GlowLine
 from explosion import ExplosionMaker
 from target_manager import TargetManager
 from swatch import Swatch
@@ -26,6 +27,9 @@ class GameView(arcade.View):
             self.window.get_size(), "glow_ball.glsl"
         )
 
+        self.glowline_shadertoy = Shadertoy.create_from_file(
+            self.window.get_size(), "glow_line.glsl"
+        )
         self.explosion_list = []
         self.target_manager = TargetManager(shadertoy=self.glowball_shadertoy)
         self.color_manager = ColorManager()
@@ -41,6 +45,10 @@ class GameView(arcade.View):
         self.score = 0
 
         self.start_new_game()
+
+        # Add sounds
+        self.laser_sound = arcade.load_sound(":resources:sounds/hurt5.wav")
+        self.hit_sound = arcade.load_sound(":resources:sounds/explosion1.wav")
 
     def start_new_game(self):
         """Set up the game and initialize the variables."""
@@ -93,10 +101,12 @@ class GameView(arcade.View):
     def fire_projectile(self, laser_color):
         """Shoot a projectile in the specified color in the direction the ship is pointing."""
 
-        laser_sprite = GlowBall(
-            glowcolor=laser_color, radius=10, shadertoy=self.glowball_shadertoy
+        laser_sprite = GlowLine(
+            glowcolor=laser_color, length=10, shadertoy=self.glowline_shadertoy
         )
         self.set_laser_vector(laser_sprite, 5)
+
+        arcade.play_sound(self.laser_sound)
 
     def set_laser_vector(self, laser_sprite, laser_speed):
         """Calculate the direction the ship is currently firing lasers"""
@@ -145,9 +155,11 @@ class GameView(arcade.View):
             for target in targets_hit:
                 if colors_match(laser.get_color(), target.get_color()):
                     self.target_manager.remove_target(target)
-                    explosion = ExplosionMaker(self.window.get_size(), target.position)
+                    explosion = ExplosionMaker(self.window.get_size(), target.position, color = target.get_color())
                     self.explosion_list.append(explosion)
                     self.score += 1
+
+                    arcade.play_sound(self.hit_sound)
 
             if len(targets_hit) > 0:
                 laser.remove_from_sprite_lists()
@@ -193,8 +205,8 @@ class GameView(arcade.View):
         self.target_manager.draw()
         self.swatch.draw()
 
-        # arcade.draw_text(
-        #     "",
-        #     start_x=SCREEN_WIDTH / 2,
-        #     start_y=SCREEN_HEIGHT / 2,
-        # )
+        arcade.draw_text(
+            self.score,
+            start_x=SCREEN_WIDTH*15/16,
+            start_y=SCREEN_HEIGHT*15/16,
+        )
